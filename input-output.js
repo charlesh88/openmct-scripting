31a082521e725442ba1b586521a96ee3560a55a2
Charles Hacskaylo <charlesh88@gmail.com>
2023-08-30 16:20:42 -0700

    Refactor towards multiple file input - Using promises for file input. - WIP.

--- input-output.js
+++ input-output.js
@@ -4,7 +4,7 @@ const inputFile = document.getElementById("inputFile");
 const inputStatsDisplay = document.getElementById("input-stats");
 const displayInputCsv = document.getElementById('display-csv');
 
-myForm.addEventListener("submit", function (e) {
+/*myForm.addEventListener("submit", function (e) {
     // https://sebhastian.com/javascript-csv-to-array/
     // Fires when the Generate button is pressed
     e.preventDefault();
@@ -15,22 +15,70 @@ myForm.addEventListener("submit", function (e) {
 
     if (input !== undefined) {
         reader.onload = function (e) {
-            if (inputType.includes('csv')) {
+            if (input.name.includes('.csv')) {
                 const data = csvToArray(e.target.result);
                 inputStatsDisplay.innerHTML = data.length + ' items found.';
                 displayInputCsv.innerHTML = JSON.stringify(data);
                 createOpenMCTJSONfromCSV(data, 'csv');
-            } else {
+            } else if (input.name.includes('.prl')) {
                 // console.log('Pride proc stuff path goes here');
                 prlToDisplayMain(e.target.result, input.name);
+            } else {
+                inputStatsDisplay.innerHTML = 'Please select a CSV or PRL file and try again.'
             }
         };
 
         reader.readAsText(input);
     } else {
-        inputStatsDisplay.innerHTML = 'Please select a telemetry objects CSV file and try again.'
+        inputStatsDisplay.innerHTML = 'Please select a CSV or PRL file and try again.'
     }
-});
+});*/
+
+/************************************* https://ourcodeworld.com/articles/read/1438/how-to-read-multiple-files-at-once-using-the-filereader-class-in-javascript#google_vignette */
+function readFileAsText(file){
+    return new Promise(function(resolve,reject){
+        let fr = new FileReader();
+
+        fr.onload = function(){
+            resolve(fr.result);
+            const filename = file.name;
+            // console.log(file.name, 'fr.onload',fr);
+            if (filename.includes('.csv')) {
+                const data = csvToArray(fr.result);
+                inputStatsDisplay.innerHTML = data.length + ' items found.';
+                displayInputCsv.innerHTML = JSON.stringify(data);
+                createOpenMCTJSONfromCSV(data, 'csv');
+            } else if (filename.includes('.prl')) {
+                // console.log('Pride proc stuff path goes here');
+                prlToDisplayMain(fr.result, filename);
+            } else {
+                inputStatsDisplay.innerHTML = 'Please select a CSV or PRL file and try again.'
+            }
+        };
+
+        fr.onerror = function(){
+            reject(fr);
+        };
+
+        fr.readAsText(file);
+    });
+}
+
+// Handle fileupload
+document.getElementById("fileinput").addEventListener("change", function(ev){
+    let file = ev.currentTarget.files[0];
+
+    // Abort file reading if no file was selected
+    if(!file) return;
+
+    readFileAsText(file).then((fileContent) => {
+        // Print file content on the console
+        console.log(fileContent);
+    });
+}, false);
+
+
+
 
 function getConfigFromForm() {
     // Get form values
